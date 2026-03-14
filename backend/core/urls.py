2e8c085fb4a9489e18df38a813d83a3b2e9d0ccc
Abhi-ram-G <itsabhiram30@gmail.com>
2026-03-14 14:44:54 +0530

    Updated Django admin URL

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from registry.views import CustomTokenView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('gapt-admin/', admin.site.urls),
     path('o/token/', CustomTokenView.as_view(), name='token'),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('api/', include('rest_framework.urls')),
